81a93632c07ee2246bc7d1dbd1de05268eb6762b
Hyerim Jeon <127684309+gofla1996@users.noreply.github.com>
2025-02-10 09:25:57 +0900

    [수정] Update items.ts 이미지 경로 수정

--- src/data/items.ts
+++ src/data/items.ts
@@ -3,21 +3,21 @@ import type { ItemListType } from '@/types/items';
 const items: ItemListType = [
   {
     id: 1,
-    img: '../src/assets/milk.png',
+    img: '../src/assets/milk.jpg',
     name: '1A 우유 900mL',
     price: 1880,
     stock: 10,
   },
   {
     id: 2,
-    img: '../src/assets/bean_sprout.png',
+    img: '../src/assets/bean_sprout.jpg',
     name: '맛있는 콩나물 500g',
     price: 1280,
     stock: 4,
   },
   {
     id: 1,
-    img: '../src/assets/bean_curd.png',
+    img: '../src/assets/bean_curd.jpg',
     name: '고소한 두부 1kg',
     price: 2280,
     stock: 7,
