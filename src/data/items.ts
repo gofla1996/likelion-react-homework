e121d91b9b8742813b8743caaa62f7916a0e0fe9
gofla1996 <110741@daum.net>
2025-02-15 23:18:14 +0900

    [수정] 이미지 링크 수정 - 이미지 소스 링크 수정

--- src/data/items.ts
+++ src/data/items.ts
@@ -3,21 +3,21 @@ import type { ItemListType } from '@/types/items';
 const items: ItemListType = [
   {
     id: 1,
-    img: '../src/assets/milk.jpg',
+    img: './src/assets/milk.jpg',
     name: '1A 우유 900mL',
     price: 1880,
     stock: 10,
   },
   {
     id: 2,
-    img: '../src/assets/bean_sprout.jpg',
+    img: './src/assets/bean_sprout.jpg',
     name: '맛있는 콩나물 500g',
     price: 1280,
     stock: 4,
   },
   {
     id: 1,
-    img: '../src/assets/bean_curd.jpg',
+    img: './src/assets/bean_curd.jpg',
     name: '고소한 두부 1kg',
     price: 2280,
     stock: 7,
