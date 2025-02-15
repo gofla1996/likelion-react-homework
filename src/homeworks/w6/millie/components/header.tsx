ed13cecfe7818e11022c8afc83b5aeb3cd6d122f
gofla1996 <110741@daum.net>
2025-02-16 04:49:51 +0900

    [수정] 밀리 로고 이미지

--- src/homeworks/w6/millie/components/header.tsx
+++ src/homeworks/w6/millie/components/header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex justify-between">
-      <img src="./millie-logo.png" alt="밀리의 서재 로고" className="w-10" />
+      <img src="./millie-logo.jpg" alt="밀리의 서재 로고" className="w-10" />
       <button type="button">로그아웃</button>
     </header>
   );
