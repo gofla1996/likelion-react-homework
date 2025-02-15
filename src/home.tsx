46debccc05cbe223357190836250a791c3703735
gofla1996 <110741@daum.net>
2025-02-16 05:23:43 +0900

    [수정] 홈페이지 마크업 구조 수정

--- src/home.tsx
+++ src/home.tsx
@@ -5,19 +5,29 @@ export default function Home() {
   return (
     <div
       className={tm(
-        'flex flex-col items-center  gap-3',
-        'my-50 mx-auto py-10 w-100',
+        'flex flex-col items-center  gap-10',
+        'my-50 mx-auto py-10 px-10 w-fit',
         'rounded-3xl shadow-lg',
         'bg-indigo-300'
       )}
     >
-      <h1 className="text-2xl font-bold text-gray-800">리액트 과제</h1>
-      <Link to="/homework5" className="hover:text-indigo-200">
-        과제 5로 이동
-      </Link>
-      <Link to="/homework6" className="hover:text-indigo-200">
-        과제 6로 이동
-      </Link>
+      <h1 className="text-2xl font-bold text-gray-800">
+        리액트 과제 by Hyerim
+      </h1>
+      <div>
+        <ul className="flex flex-col gap-3">
+          <li>
+            <Link to="/homework5" className="hover:text-indigo-200">
+              5주차 과제 : 쇼핑 카트 구현
+            </Link>
+          </li>
+          <li>
+            <Link to="/homework6" className="hover:text-indigo-200">
+              6주차 과제 : 카드 검색 리스트 구현 - 밀리의 서재
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
